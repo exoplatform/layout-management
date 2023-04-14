bcc150ef9439fbf3ea11caebdef0ea0f9dc7a7fb
GouadriaHanen <hgouadria@exoplatform.com>
2023-04-14 11:19:12 +0100

    feat: manage access edit permissions - EXO-63142 - Meeds-io/meeds#51

--- layout-management-webapps/src/main/webapp/vue-app/site-navigation/siteNavigationService.js
+++ layout-management-webapps/src/main/webapp/vue-app/site-navigation/siteNavigationService.js
@@ -86,13 +86,15 @@ export function updateNodePagePermission(pageRef, editPermission, accessPermissi
 
   const formData = new FormData();
 
+  formData.append('pageRef', pageRef);
+
   formData.append('editPermission', editPermission);
 
   formData.append('accessPermissions', accessPermissions);
 
   const params = new URLSearchParams(formData).toString();
 
-  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/siteNavigation/page/permissions/${pageRef}?${params}`, {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/siteNavigation/page/permissions?${params}`, {
     method: 'PATCH',
     credentials: 'include',
   }).then((resp) => {
