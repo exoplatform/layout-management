c3dea271bac04f32e3cd61eb9bea0438d24bd3fa
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2023-04-17 17:06:12 +0100

    feat: Display Edit layout action only for users having page edit permission - EXO-63629 - Meeds-io/meeds#51 (#71)

--- layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/NodeItemMenu.vue
+++ layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/NodeItemMenu.vue
@@ -34,7 +34,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
     </template>
     <v-list class="pa-0" dense>
       <v-list-item
-        v-if="navigationNode.pageKey"
+        v-if="canEditPage"
         class="subtitle-2" 
         @click="editLayout">
         <v-icon
