e4a5163b3c5a3bbe92d8f655ea7ba334882b649b
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2023-05-03 18:01:57 +0200

    clean code

--- layout-management-services/src/main/java/org/exoplatform/layoutmanagement/rest/SiteNavigationRestService.java
+++ layout-management-services/src/main/java/org/exoplatform/layoutmanagement/rest/SiteNavigationRestService.java
@@ -384,7 +384,7 @@ public class SiteNavigationRestService implements ResourceContainer, Startable {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @RolesAllowed("users")
-  @Operation(summary = "Retrieve pages", method = "GET", description = "this retrieves pages")
+  @Operation(summary = "Retrieve pages", method = "GET", description = "This retrieves pages")
   @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Request fulfilled"),
       @ApiResponse(responseCode = "500", description = "Internal server error"), })
   public Response getPages(@Context
