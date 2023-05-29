1e9cf0afb2a6e6ac708b9fbe4472ca2bbedde752
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2023-05-29 12:55:55 +0100

    feat: Implement Last updated column in Expand full view mode of Site Navigation - EXO-64489 - Meeds-io/MIPs#51 (#109)

--- layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/NodeListTableItem.vue
+++ layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/NodeListTableItem.vue
@@ -61,6 +61,11 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
         </span>
       </td>
       <td class="align-center">
+        <span>
+          {{ lastUpdatedDate }}
+        </span>
+      </td>
+      <td class="align-center">
         <v-icon
           :title="visibilityIcon.title"
           color="grey"
@@ -117,7 +122,15 @@ export default {
     return {
       displayChildren: false,
       nodeToPaste: null,
-      pasteMode: null
+      pasteMode: null,
+      lang: eXo.env.portal.language || 'en',
+      fullDateFormat: {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      },
     };
   },
   computed: {
@@ -170,6 +183,9 @@ export default {
           title: this.$t('siteNavigation.label.access.specific'),
         };
       }
+    },
+    lastUpdatedDate() {
+      return this.formatDate(this.navigationNode.lastModifiedDate);
     },
   },
   created() {
@@ -235,7 +251,10 @@ export default {
     copyNode(navigationNode) {
       this.pasteMode = 'Copy';
       this.nodeToPaste = navigationNode;
-    }
+    },
+    formatDate(time) {
+      return !time && '--' || this.$dateUtil.formatDateObjectToDisplay(new Date(time),this.fullDateFormat, this.lang);
+    },
   }
 };
 </script>
