69c3220ac565ae11085a52156faf8f8ed28e0332
GouadriaHanen <hgouadria@exoplatform.com>
2023-05-31 10:16:24 +0100

    refactor code

--- layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/SiteNavigationNodeDrawer.vue
+++ layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/SiteNavigationNodeDrawer.vue
@@ -425,7 +425,7 @@ export default {
       }
     },
     urlVerify(url) {
-      if (!url.match(/^((https?:\/\/))|(javascript:)|(\/portal\/)/)) {
+      if (!url.match(/^(https?:\/\/|javascript:|\/portal\/)/)) {
         url = `//${url}`;
       }
       return url ;
