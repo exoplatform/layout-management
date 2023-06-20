ae4c9330b96673efcefbe11efff2c3c250ccb3ed
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2023-06-20 15:35:56 +0100

    feat: use extension registry to add Site Navigation icon on Manage spaces - EXO-64650 - Meeds-io/MIPs#68 (#153)

---
+++ layout-management-webapps/src/main/webapp/vue-app/site-navigation/components/SiteNavigationButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-tooltip bottom>
+    <template #activator="{ on, attrs }">
+      <v-btn
+        size="16"
+        outlined
+        icon
+        class="mx-2">
+        <v-icon
+          v-on="on"
+          v-bind="attrs"
+          class="mb-2"
+          :color="iconColor"
+          :class="iconClass"
+          size="16"
+          @click="openSiteNavigationDrawer">
+          fas fa-sitemap
+        </v-icon>
+      </v-btn>
+    </template>
+    <span>{{ $t('siteNavigation.button.tooltip.label') }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    iconClass: {
+      type: String,
+      default: null,
+    },
+    iconColor: {
+      type: String,
+      default: null,
+    },
+    siteName: {
+      type: String,
+      default: null,
+    },
+    siteType: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {
+    openSiteNavigationDrawer() {
+      let params;
+      if (this.siteName && this.siteType) {
+        params = {
+          siteName: this.siteName,
+          siteType: this.siteType,
+        };
+      }
+      document.dispatchEvent(new CustomEvent('open-site-navigation-drawer',{detail: params}));
+    }
+  }
+};
+</script>
