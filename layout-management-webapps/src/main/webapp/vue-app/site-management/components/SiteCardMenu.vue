99da27dfb7e92f80f211eea7bc792649831e3f85
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2023-07-07 14:14:05 +0100

    feat: remove duplicated computed key - EXO-64952 - Meeds-io/meeds#68 (#175)

--- layout-management-webapps/src/main/webapp/vue-app/site-management/components/SiteCardMenu.vue
+++ layout-management-webapps/src/main/webapp/vue-app/site-management/components/SiteCardMenu.vue
@@ -32,35 +32,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
     </template>
     <v-list class="pa-0" dense>
       <v-list-item
-        class="subtitle-2 px-3"
-        @click="openSiteNavigationDrawer">
-        <v-icon
-          size="13"
-          class="me-2 ms-0"
-          color="primary">
-          fas fa-sitemap
-        </v-icon>
-        <v-list-item-title
-          class="subtitle-2">
-          <span class="ps-1">{{ $t('siteManagement.label.navigation') }}</span>
-        </v-list-item-title>
-      </v-list-item>
-      <v-list-item
-        v-if="canDelete"
-        class="subtitle-2 px-3"
-        @click="$root.$emit('delete-site', site)">
-        <v-icon
-          size="13"
-          class="me-2 ms-0"
-          color="primary">
-          fas fa-trash
-        </v-icon>
-        <v-list-item-title
-          class="subtitle-2">
-          <span class="ps-1">{{ $t('siteManagement.label.delete') }}</span>
-        </v-list-item-title>
-      </v-list-item>
-      <v-list-item
         v-if="isPortalSite"
         class="subtitle-2 px-3"
         @click="openSiteCardPropertiesDrawer">
@@ -68,6 +39,35 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
         <v-list-item-title
           class="subtitle-2">
           <span class="ps-1">{{ $t('siteManagement.label.properties') }}</span>
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item
+          class="subtitle-2 px-3"
+          @click="openSiteNavigationDrawer">
+        <v-icon
+            size="13"
+            class="me-2 ms-0"
+            color="primary">
+          fas fa-sitemap
+        </v-icon>
+        <v-list-item-title
+            class="subtitle-2">
+          <span class="ps-1">{{ $t('siteManagement.label.navigation') }}</span>
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item
+          v-if="canDelete"
+          class="subtitle-2 px-3"
+          @click="$root.$emit('delete-site', site)">
+        <v-icon
+            size="13"
+            class="me-2 ms-0"
+            color="primary">
+          fas fa-trash
+        </v-icon>
+        <v-list-item-title
+            class="subtitle-2">
+          <span class="ps-1">{{ $t('siteManagement.label.delete') }}</span>
         </v-list-item-title>
       </v-list-item>
     </v-list>
@@ -97,8 +97,10 @@ export default {
     },
     canDelete() {
       return !(this.isDefaultPortalSite || this.isGlobalSite || this.isGroupSite);
+    },
+    isPortalSite(){
+      return this.site.siteType === 'PORTAL';
     }
-
   },
   created() {
     $(document).on('mousedown', () => {
@@ -108,11 +110,6 @@ export default {
         }, 200);
       }
     });
-  },
-  computed: {
-    isPortalSite(){
-      return this.site.siteType === 'PORTAL';
-    }
   },
   methods: {
     openSiteNavigationDrawer() {
