56026626138048495ecca616687a8e3d9d610fb1
IlhemEssaadi <iessaadi@exoplatform.com>
2023-12-20 12:32:59 +0100

    fix review

--- layout-management-webapps/src/main/webapp/vue-app/site-navigation/main.js
+++ layout-management-webapps/src/main/webapp/vue-app/site-navigation/main.js
@@ -44,6 +44,8 @@ const lang = eXo && eXo.env.portal.language || 'en';
 //should expose the locale ressources as REST API
 const urls = [`${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.layoutManagement.SiteNavigationPortlet-${lang}.json`];
 
+exoi18n.loadLanguageAsync(lang, urls);
+
 export function init(canManageSiteNavigation) {
   exoi18n.loadLanguageAsync(lang, urls).then(i18n => {
     // init Vue app when locale ressources are ready
